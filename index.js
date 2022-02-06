b126d4b19540692972d8daed8d9f720d38e1d8e2
alksdjesus <56933580+alksdjesus@users.noreply.github.com>
2022-02-06 02:39:42 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//index.js file for discord bot
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+client.login('token');
