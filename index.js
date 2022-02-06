959177194e90e179fa5cd124bc3ee852b03049d0
alksdjesus <56933580+alksdjesus@users.noreply.github.com>
2022-02-06 04:34:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+    if (msg.content === 'Peng') {
+        msg.reply('Huang');
     }
 });
 
